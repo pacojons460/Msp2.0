ae1c4096a5220014da76f1a42707ac73ec920123
pacojons460 <posduck88@gmail.com>
2024-08-13 20:28:44 +0000

    Mundo guardado 2024-08-13

--- server.py
+++ server.py
@@ -7,7 +7,8 @@
 # Puedes instalar purpur después de instalar fabric desde el menú de gestionar
 
 # Regiones de ngrok
-# Código          Lugar
+# Código          Lugar1
+
 #-----------      ---------------------------
 # ap	          Asia / Pacífico (Singapore)
 # au		      Australia (Sydney)
